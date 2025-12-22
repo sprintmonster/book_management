0915e297f91f29ec7349e36cc06bd42a413ead5b
jeon125 <eugine2001@gmail.com>
2025-12-22 17:15:55 +0900

    WebConfig import 추가

--- backend/demo/src/main/java/com/kt_miniproject/demo/WebConfig.java
+++ backend/demo/src/main/java/com/kt_miniproject/demo/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
